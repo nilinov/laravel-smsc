92f616ea1689976c9ab03cc16422031939a47cf6
Dmitriy K <dmitriy.kim@mail.ru>
2021-03-18 16:49:40 +0600

    New middleware for validate tokens

---
+++ src/Middlewares/TokenizedPhoneNumber.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Papalapa\Laravel\Smsc\Middlewares;
+
+use Illuminate\Http\Request;
+use Papalapa\Laravel\Smsc\PhoneNumber;
+use Papalapa\Laravel\Smsc\Services\TokenGenerator;
+
+final class TokenizedPhoneNumber
+{
+    public const TOKENIZED_PHONE_NUMBER = 'tokenizedPhoneNumber';
+
+    public function __construct(
+        private TokenGenerator $tokenGenerator
+    ) {
+    }
+
+    public static function getPhoneNumber(Request $request): PhoneNumber
+    {
+        return $request->attributes->get(self::TOKENIZED_PHONE_NUMBER);
+    }
+
+    public function handle(Request $request, \Closure $next)
+    {
+        $token = $request->input('token');
+        $phoneNumber = $this->tokenGenerator->validate($token);
+        $request->attributes->set(self::TOKENIZED_PHONE_NUMBER, $phoneNumber);
+
+        return $next($request);
+    }
+}
