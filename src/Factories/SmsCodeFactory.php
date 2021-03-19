ad11069dc67440a6444bd1e794662a8fc27c2bdd
Dmitriy K <dmitriy.kim@mail.ru>
2021-03-19 11:01:54 +0600

    SmsCodeFactory.php refactoring

--- src/Factories/SmsCodeFactory.php
+++ src/Factories/SmsCodeFactory.php
@@ -7,40 +7,32 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Model;
 use Papalapa\Laravel\Smsc\Models\SmsCode;
+use Papalapa\Laravel\Smsc\PhoneNumber;
+use Papalapa\Laravel\Smsc\Services\CodeGenerator;
 
 final class SmsCodeFactory extends Factory
 {
     protected $model = SmsCode::class;
 
-    public function create($attributes = [], ?Model $parent = null) : Collection|SmsCode
+    public function create($attributes = [], ?Model $parent = null): Collection|SmsCode
     {
         return parent::create($attributes, $parent);
     }
 
-    public function make($attributes = [], ?Model $parent = null) : Collection|SmsCode
+    public function make($attributes = [], ?Model $parent = null): Collection|SmsCode
     {
         return parent::make($attributes, $parent);
     }
 
-    public function definition() : array
+    public function definition(): array
     {
         return [
             'id' => $this->faker->uuid,
-            'number' => $this->generateNumber(),
-            'code' => $this->generateCode(),
+            'number' => PhoneNumber::asNumeric($this->faker->phoneNumber),
+            'code' => CodeGenerator::generateStatic(),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
             'deleted_at' => null,
         ];
-    }
-
-    private function generateNumber() : string
-    {
-        return sprintf('77%09d', $this->faker->numberBetween(0, 10 ** 9 - 1));
-    }
-
-    private function generateCode() : string
-    {
-        return sprintf('%06d', $this->faker->numberBetween(0, 10 ** 6 - 1));
     }
 }
