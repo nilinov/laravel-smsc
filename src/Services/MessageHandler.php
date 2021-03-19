96c419fd51ad4974715cb75ef837473f3a19f170
Dmitriy K <dmitriy.kim@mail.ru>
2021-03-19 11:04:12 +0600

    MessageHandler.php code optimization

--- src/Services/MessageHandler.php
+++ src/Services/MessageHandler.php
@@ -16,39 +16,39 @@ final class MessageHandler
     ) {
     }
 
-    public function sendCode(PhoneNumber $phoneNumber) : SmsMessage
+    public function sendCode(PhoneNumber $phoneNumber): SmsMessage
     {
         $smsCode = $this->codeCreator->create($phoneNumber);
-        $message = sprintf('Код подтверждения: %s', $smsCode->code);
+        $message = sprintf('Verification code: %s', $smsCode->code);
 
         return $this->send($phoneNumber, $message);
     }
 
-    public function validateCode(PhoneNumber $tel, string $code) : bool
-    {
-        return $this->codeChecker->check($tel, $code);
-    }
-
-    public function generateToken(PhoneNumber $tel) : string
-    {
-        return $this->tokenGenerator->generate($tel);
-    }
-
-    public function validateToken(string $data) : PhoneNumber
-    {
-        return $this->tokenGenerator->validate($data);
-    }
-
-    public function sendMessage(PhoneNumber $tel, string $message) : SmsMessage
-    {
-        return $this->send($tel, $message);
-    }
-
-    private function send(PhoneNumber $tel, string $message) : SmsMessage
+    private function send(PhoneNumber $tel, string $message): SmsMessage
     {
         $sms = new SmsMessage($tel, $message);
         $this->sender->send($sms);
 
         return $sms;
+    }
+
+    public function validateCode(PhoneNumber $tel, string $code): bool
+    {
+        return $this->codeChecker->check($tel, $code);
+    }
+
+    public function generateToken(PhoneNumber $tel): string
+    {
+        return $this->tokenGenerator->generate($tel);
+    }
+
+    public function validateToken(string $data): PhoneNumber
+    {
+        return $this->tokenGenerator->validate($data);
+    }
+
+    public function sendMessage(PhoneNumber $tel, string $message): SmsMessage
+    {
+        return $this->send($tel, $message);
     }
 }
